6c25dd620679153e2c6d65a0c8a5b0f0fd9794df
AshokReddyGodi <ashokgodi1996@gmail.com>
2019-05-27 16:52:13 +0530

    Created loginform in angular

---
+++ src/app/formvalidation/formvalidation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formvalidation',
+  templateUrl: './formvalidation.component.html',
+  styleUrls: ['./formvalidation.component.css']
+})
+export class FormvalidationComponent implements OnInit {
+  model: any = {}
+
+  constructor() { }
+
+  ngOnInit() {
+    alert(JSON.stringify(this.model))
+  }
+
+}
